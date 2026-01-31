3250deff90bcb132f65ff61f7c8ad612affa5196
MartinsFlorian <f.martins2005.fm@gmail.com>
2026-01-31 19:59:38 +0100

    Fix: SSO

--- Assets/Scripts/Runtime/Wrapper/SSO/SSO_ListPattern.cs
+++
@@ -1,5 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SSO_ListPattern", menuName = "Data/SSO/Pattern/ListPattern")]
-public class SSO_ListPattern : BT.ScriptablesObject.StaticScriptableObject<List<S_ClassPattern>>{}
